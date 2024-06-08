d529b2db52ba74e21151e4b19142537adb435e2f
kromate <akpananthony33@gmail.com>
2024-06-08 10:44:35 +0100

    -

--- src/composables/core/modals/index.ts
+++ src/composables/core/modals/index.ts
@@ -2,17 +2,19 @@ import { useModal } from '../modal'
 
 // ==================== AUTH ===============================
 import Logout from '@/components/modals/auth/logout.vue'
-import 
 
+// ==================== CORE ===============================
 
+import Confirmation from '@/components/modals/core/Confirmation.vue'
 
 
 
 type AuthTypes = 'Logout'
+type CoreTypes = 'Confirmation'
 
 
 const AuthModals = { Logout } as Record<AuthTypes, any>
-
+const CoreModals = { Confirmation } as Record<CoreTypes, any>
 
 
 export const modal = useModal()
